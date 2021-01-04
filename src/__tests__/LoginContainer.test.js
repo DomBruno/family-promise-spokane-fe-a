effb5b8253118f8a05374e92c0c22b4e5b96186f
Abdi Mohamud <abdinajibmohamud@gmail.com>
2021-01-04 09:51:08 -0800

    added provider and redux this time in okta Login test

--- src/__tests__/LoginContainer.test.js
+++ src/__tests__/LoginContainer.test.js
@@ -3,14 +3,24 @@ import { useHistory } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { SecureRoute, Security } from '@okta/okta-react';
 import { config } from '../utils/oktaConfig';
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import { rootReducer } from '../state/reducers/index';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import LoginContainer from '../components/pages/Login/LoginContainer';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 describe('<LoginContainer /> test suite', () => {
-  test('signin widget mounts successfully', () => {
+  test('signin widget mounts successfully', async () => {
     const { container } = render(
       <Security {...config} onAuthRequired={() => useHistory().push('/login')}>
-        <LoginContainer />
+        <Provider store={store}>
+          <Router>
+            <LoginContainer />
+          </Router>
+        </Provider>
       </Security>
     );
     expect(container.querySelector('#sign-in-widget')).toBeTruthy();
